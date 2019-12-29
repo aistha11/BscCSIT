aa607482224c62aa835bb99d4ca85e0d914d280f
Bijay <bijayshrestha0908@gmail.com>
2019-12-29 16:29:29 +0545

    multimedia lab

--- sem5/Multimedia/lab1/animatedcircle.cpp
+++ sem5/Multimedia/lab1/animatedcircle.cpp
@@ -2,20 +2,29 @@
 #include<iostream>
 #include<stdlib.h>
 #include<iomanip>
-#define n 100
+#define n 10
 using namespace std;
+
+void draw(int cx, int cy, int r)
+{
+       int i,gap=0;
+       for(i=0;i<n;i++)
+       {
+           setcolor(rand()%10);
+            circle(cx,cy,r+gap);
+            gap+=10;
+       }
+       delay(100);
+       cleardevice();
+}
 int main()
 {
    int gdriver=DETECT,gmode;
    initgraph(&gdriver, &gmode, "");
-   int cx=300,cy=200,r=10,i;
-   for(i=0;i<n;i++)
+   int cx=300,cy=200,r=10;
+   while(1)
    {
-       setcolor(rand()%10);
-       circle(cx,cy,r);
-       r=r+1;
-       delay(100);
-       cleardevice();
+        draw(cx,cy,r);
    }
    getch();
    closegraph();
---
+++ sem5/Multimedia/lab1/polygon.cpp
@@ -0,0 +1,2 @@
+//make a polygon split it into different parts and fill with different colors
+//open ms paint . Draw your signature and save in .jpeg with different depth, resolution, and compare it.

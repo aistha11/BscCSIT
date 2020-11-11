01a940b22fbc519969a299e3c2fbb3e8cf6491be
aistha11 <bijayshrestha0908@gmail.com>
2020-11-11 12:00:06 +0545

    assignment and labs

--- sem6/NCC/assignments/Assignment1/AsyncTest.cs
+++ sem6/NCC/assignments/Assignment1/AsyncTest.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Assignment1
 {
     class AsyncTest
     {
+        //public static async Task Main()
+        //{
+        //    await foreach (var item in MessageAsync())
+        //    {
+        //        System.Console.WriteLine(item);
+        //    }
+        //    Console.ReadKey();
+        //}
+        //static async IAsyncEnumerable<string> MessageAsync()
+        //{
+        //    await Task.Delay(2000);
+        //    yield return "Hello!";
+        //    await Task.Delay(2000);
+        //    yield return "Hello!";
+        //}
     }
 }
